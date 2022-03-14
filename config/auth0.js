fc06f56ac4b845458beee007dae2af62e470ad14
Saim <75107444+iamsaimsheikh@users.noreply.github.com>
2022-03-14 10:27:50 +0500

    Update auth0.js

--- config/auth0.js
+++ config/auth0.js
@@ -4,7 +4,7 @@ const auth0Config = {
         authRequired: false,
         auth0Logout: true,
         issuerBaseURL: process.env.ISSUER_BASE_URL,
-        baseURL: process.env.BASE_URL,
+        baseURL: "https://nova-movie-db.herokuapp.com/",
         clientID: process.env.CLIENT_ID,
         secret: process.env.SECRET,
       }
